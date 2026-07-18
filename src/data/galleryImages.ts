81621d6c8aed240adc6cc05c21c58abddadeabd2
khazstack <khazstack@gmail.com>
2026-07-18 10:11:19 +0500

    Add new photos to galleryImages array

--- src/data/galleryImages.ts
+++ src/data/galleryImages.ts
@@ -10,18 +10,34 @@ import photo7 from "@/assets/photo7.jpeg"
 import photo8 from "@/assets/photo8.jpeg"
 import photo9 from "@/assets/photo9.jpeg"
 import photo10 from "@/assets/photo10.jpeg"
+import photo11 from "@/assets/photo11.jpeg"
+import photo12 from "@/assets/photo12.jpeg"
+import photo13 from "@/assets/photo13.jpeg"
+import photo14 from "@/assets/photo14.png"
+import photo15 from "@/assets/photo15.jpeg"
+import photo16 from "@/assets/photo16.jpeg"
+import photo17 from "@/assets/photo17.jpeg"
+import photo18 from "@/assets/photo18.jpeg"
 
 export const galleryImages: MediaItem[] = [
-  { src: photo1, width: 9, height: 16, alt: "Фото в жёлтом платье" },
-  { src: photo2, width: 9, height: 16, alt: "Фото в ярком кимоно" },
-  { src: photo3, width: 9, height: 16, alt: "Портрет с чёрным цветком" },
-  { src: photo4, width: 16, height: 9, alt: "Фото сверху в кожаной куртке" },
-  { src: photo5, width: 16, height: 9, alt: "Фото в кимоно с поднятыми руками" },
-  { src: photo6, width: 9, height: 16, alt: "Портрет в жёлтом платье" },
-  { src: photo7, width: 9, height: 16, alt: "Портрет за стулом" },
-  { src: photo8, width: 9, height: 16, alt: "Фото в национальном чапане" },
-  { src: photo9, width: 16, height: 9, alt: "Фото сверху в белой рубашке" },
-  { src: photo10, width: 16, height: 9, alt: "Портрет в национальном чапане" },
+  { src: photo1, width: 9, height: 16, alt: "Портрет 1" },
+  { src: photo2, width: 9, height: 16, alt: "Портрет 2" },
+  { src: photo3, width: 9, height: 16, alt: "Портрет 3" },
+  { src: photo4, width: 9, height: 16, alt: "Портрет 4" },
+  { src: photo5, width: 9, height: 16, alt: "Портрет 5" },
+  { src: photo6, width: 9, height: 16, alt: "Портрет 6" },
+  { src: photo7, width: 9, height: 16, alt: "Портрет 7" },
+  { src: photo8, width: 9, height: 16, alt: "Портрет 8" },
+  { src: photo9, width: 9, height: 16, alt: "Портрет 9" },
+  { src: photo10, width: 9, height: 16, alt: "Портрет 10" },
+  { src: photo11, width: 9, height: 16, alt: "Портрет 11" },
+  { src: photo12, width: 9, height: 16, alt: "Портрет 12" },
+  { src: photo13, width: 9, height: 16, alt: "Портрет 13" },
+  { src: photo14, width: 9, height: 16, alt: "Портрет 14" },
+  { src: photo15, width: 9, height: 16, alt: "Портрет 15" },
+  { src: photo16, width: 9, height: 16, alt: "Портрет 16" },
+  { src: photo17, width: 9, height: 16, alt: "Портрет 17" },
+  { src: photo18, width: 9, height: 16, alt: "Портрет 18" },
 ]
 
 export const introImages: string[] = [
